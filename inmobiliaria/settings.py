fc85a4184b8a209683cf760cc89cdcd3c5373275
Maria Fernanda Ney <maferney0103@gmail.com>
2024-11-28 16:21:41 -0300

    se cambia el settings

--- inmobiliaria/settings.py
+++ inmobiliaria/settings.py
@@ -132,10 +132,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 if not DEBUG:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static'),
-]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
